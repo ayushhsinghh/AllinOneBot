94c2cb4cf11f5c2950b23092285d18fb775f6bf8
Ayush Singh <Ayushsingh12525@gmail.com>
2021-02-03 15:47:40 +0530

    updated heruko url

--- bot.py
+++ bot.py
@@ -58,7 +58,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=token)
-    updater.bot.setWebhook('https://yourherokuappname.herokuapp.com/' + token)
+    updater.bot.setWebhook('https://cybergod-telegram-bot.herokuapp.com/' + token)
     updater.idle()
 
 if __name__ == '__main__':
