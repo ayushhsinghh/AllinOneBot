03f29f90d3a6f0618e0237713505c5db26a49bfe
root <ayushsingh1525@gmail.com>
2021-07-03 16:24:11 +0000

    added udemy free coupon

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from telegram.ext import Updater, InlineQueryHandler, CommandHandler
 from credentials import token 
 from Newsfun import *
 from memesfuns import get_memes , memes
+from udemyCoupon import free_udemy_coupon
 from cryptoPrice import get_crypto_price , crypto_price
 from vaccineUpdate import vaccineUpdate , vaccineUpdatedelhi
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
@@ -57,6 +58,8 @@ def help(update , context):
 
 /tech_news : Tech. News
 
+/udemycoupon : get links Paid Udemy courses that are FREE now
+
 Adding more Commands Soon...
   '''
   update.effective_message.reply_text(commandss)
@@ -82,6 +85,7 @@ def main():
     dp.add_handler(CommandHandler('getvaccine',vaccineUpdate))
     dp.add_handler(CommandHandler('vaccinedelhi',vaccineUpdatedelhi))
     dp.add_handler(CommandHandler('trend_news',trend_news))
+    dp.add_handler(CommandHandler('udemycoupon',free_udemy_coupon))
     dp.add_handler(CommandHandler('crypto_price',crypto_price))
     dp.add_error_handler(error)
     updater.start_polling()
