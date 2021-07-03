4012a7185804c7f30b616849d0c6c036d9cd3eec
root <ayushsingh1525@gmail.com>
2021-07-03 16:25:19 +0000

    full log path added

--- upload.py
+++ upload.py
@@ -1,3 +1,3 @@
 import boto3
 client = boto3.client('s3')
-client.upload_file("telegram.log" , "allinone-telegram-bot-log" , "telegram.log")
+client.upload_file("/root/telegram-bot/cybergod/telegram.log" , "allinone-telegram-bot-log" , "telegram.log")
