b6b8ad3b2fc405f3664f8c733e01c4e8ec659ef7
root <ayushsingh1525@gmail.com>
2021-09-13 16:02:07 +0000

    udemy coupon Grabber

--- udemyCoupon.py
+++ udemyCoupon.py
@@ -15,7 +15,8 @@ This backend API is hosted on heroku. Don't overuse it.
 '''
 def free_udemy_coupon(update , context):
   name = update.effective_user.first_name
-  logger.info("User %s started the Udemy Coupon Command.", name)
+  chat_id = update.message.chat_id
+  logger.info(f"User {name}:{chat_id} started the Udemy Coupon Command.")
   res = requests.get("https://freeudemycourse.herokuapp.com/").json()
   for idx , data in enumerate(res):
     update.effective_message.reply_text(f"{idx + 1}: {data['Name']} : {data['Link']}")
