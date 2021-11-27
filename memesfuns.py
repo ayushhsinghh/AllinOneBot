a5fa3c536764c35f814d763380a5965587d4adfc
Ayush Singh <ayushsingh1525@gmail.com>
2021-11-27 22:15:16 +0530

    updated URL

--- memesfuns.py
+++ memesfuns.py
@@ -1,8 +1,8 @@
 from telegram.ext import Updater, InlineQueryHandler, CommandHandler
-import requests as rq
-from bs4 import BeautifulSoup
+import requests
 from datetime import date
-from Newsfun import *
+from requests.structures import CaseInsensitiveDict
+
 
 
 import logging
@@ -15,25 +15,33 @@ logging.basicConfig( filename='telegram.log',
 logger = logging.getLogger(__name__)
 
 def get_memes():
-  session = rq.Session()
-  response = session.get("https://memechat.app/index.html")
-  soup = BeautifulSoup(response.content, 'html.parser')
-  content_class =  soup.find_all("div", {"class": "portfolio_grid no-padding"})
-  images = []
-  image_url = []
-  i = 0
-  for img in content_class:
-    images.append(img.find_all('img' , {"alt": "Portfolio"}))
-    image_url.append(images[i][0]['src'])
-    i = i + 1
-  return image_url
+  url = "https://api.memes.com/api/trending/feed?page=1"
+  headers = CaseInsensitiveDict()
+  headers["authority"] = "api.memes.com"
+  headers["Content-Length"] = "0"
+  headers["accept"] = "*/*"
+  headers["sec-ch-ua-mobile"] = "?1"
+  headers["user-agent"] = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36"
+  headers["origin"] = "https://memes.com"
+  headers["sec-fetch-site"] = "same-site"
+  headers["sec-fetch-mode"] = "cors"
+  headers["sec-fetch-dest"] = "empty"
+  headers["referer"] = "https://memes.com/"
+  headers["accept-language"] = "en-US,en;q=0.9"
+  
+  
+  resp = requests.post(url, headers=headers).json()['posts']
+  return resp
 
 
 def memes(update , context):
     name = update.effective_user.first_name
     logger.info("User %s started the Memes Command.", name)
+    base_url = "https://cdn.memes.com/"
     memes = get_memes()
     chat_id = update.effective_message.chat_id
-    for img in memes:
-      context.bot.send_photo(chat_id=chat_id, photo=img)
-    update.effective_message.reply_text("These Memes are From https://memechat.app/")
+    for idx , data in enumerate(memes):
+      if data['mediaType'] == 1:
+        img_url = base_url + data['path']
+        context.bot.send_photo(chat_id=chat_id, photo=img_url)
+    update.effective_message.reply_text("These Memes are From https://memes.com")
