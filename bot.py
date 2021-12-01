1790a2cffec704172cf60eb709a34dc006d63f98
Ayush Singh <ayushsingh1525@gmail.com>
2021-12-01 20:59:05 +0530

    testing message handler

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from telegram.ext import Updater, InlineQueryHandler, CommandHandler
 from credentials import token 
-from Newsfun import *
+# from Newsfun import *
 from memesfuns import get_memes , memes
 from torrent import torrent
 from udemyCoupon import free_udemy_coupon
@@ -10,6 +10,7 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import (
     Updater,
     CommandHandler,
+    MessageHandler,
     InlineQueryHandler,
     CallbackQueryHandler,
     ConversationHandler,
@@ -67,6 +68,14 @@ Adding more Commands Soon...
   '''
   update.effective_message.reply_text(commandss)
 
+def echo(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
+    name = update.effective_user.first_name
+    chat_id = update.message.chat_id
+    user_says = update.message.text
+    logger.info(f"User {name}:{chat_id} send message to bot : '{user_says}'")
+    update.message.reply_text(update.message.text)
+
 
 
 def error(update, context):
@@ -75,7 +84,7 @@ def error(update, context):
 
 
 def main():
-    updater = Updater(token)  # create Credentials.py and add token
+    updater = Updater(token , use_context=True)  # create Credentials.py and add token
     dp = updater.dispatcher
     
     # Handler Are Defined Below
@@ -83,11 +92,12 @@ def main():
     dp.add_handler(CommandHandler('start',start))
     dp.add_handler(CommandHandler('help',help))
     dp.add_handler(CommandHandler('memes',memes))
-    dp.add_handler(CommandHandler('tech_news',tech_news))
-    dp.add_handler(CommandHandler('ent_news',ent_news))
+    # dp.add_handler(CommandHandler('tech_news',tech_news))
+    # dp.add_handler(CommandHandler('ent_news',ent_news))
     dp.add_handler(CommandHandler('getvaccine',vaccineUpdate))
     dp.add_handler(CommandHandler('vaccinedelhi',vaccineUpdatedelhi))
-    dp.add_handler(CommandHandler('trend_news',trend_news))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    # dp.add_handler(CommandHandler('trend_news',trend_news))
     dp.add_handler(CommandHandler('torrent' , torrent))
     dp.add_handler(CommandHandler('udemycoupon',free_udemy_coupon))
     dp.add_handler(CommandHandler('crypto_price',crypto_price))
